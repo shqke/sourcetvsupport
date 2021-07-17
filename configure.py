bd112728f94c2d4cddf4e9129576491fe3425a2e
Evgeniy Kazakov <root@wampa.ru>
2021-07-17 12:09:45 +0300

    Updated GA workflow

--- configure.py
+++ configure.py
@@ -14,7 +14,7 @@ builder.options.add_option('--enable-debug', action='store_const', const='1', de
                        help='Enable debugging symbols')
 builder.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
                        help='Enable optimization')
-builder.options.add_option('-s', '--sdks', default='all', dest='sdks',
+builder.options.add_option('-s', '--sdks', default='present', dest='sdks',
                        help='Build against specified SDKs; valid args are "all", "present", or '
                             'comma-delimited list of engine names (default: %default)')
 
